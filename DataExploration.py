c8ea9fdf435be397ed7cde3567cf56b14c39a490
Mahitha Valluru <mahitha.valluru@gmail.com>
2022-08-01 20:44:48 -0400

    encountered issue
    it looks like the requests library is giving me this error: "ConnectionError: HTTPSConnectionPool(host='www.thestreet.com', port=443): Max retries exceeded with url: /jim-cramer/mad-money (Caused by NewConnectionError('<urllib3.connection.HTTPSConnection object at 0x000001DDF7FBA3A0>: Failed to establish a new connection: [WinError 10060] A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond'))"

--- DataExploration.py
+++ DataExploration.py
@@ -1,3 +1,35 @@
 # https://www.thestreet.com/jim-cramer/mad-money
 
 # How do I access this site?
+
+#from selenium import webdriver
+#from BeautifulSoup import BeautifulSoup
+#import pandas as pd
+
+url = "https://www.thestreet.com/jim-cramer/mad-money"
+
+# from bs4 import BeautifulSoup as bs
+# import pandas as pd
+# pd.set_option('display.max_colwidth', 500)
+# import time
+# import requests
+# import random
+
+# page = requests.get("https://www.thestreet.com/jim-cramer/mad-money")
+
+
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+
+session.get(url)
+
+WebClient client = new WebClient()
+client.OpenRead(myUri)
